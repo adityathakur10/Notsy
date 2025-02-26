b8d3cc6b19e2baafab3d6b6a71162b3746a57329
timetooth <95702544+timetooth@users.noreply.github.com>
2025-02-26 18:00:10 +0530

    backend added

---
+++ Backend/app.js
@@ -0,0 +1,69 @@
+<<<<<<< HEAD
+const express =require("express")
+const app=express()
+const connectDB=require('./db/connect')
+const authenticateUser = require('./middlewares/authenticate');
+require('dotenv').config();
+
+
+//middlewares
+app.use(express.json())
+
+//import routes
+const uploadRoutes=require('./routes/upload')
+const authRoutes=require('./routes/auth')
+//routes
+app.use('/notsy/auth',authRoutes)
+app.use('/notsy',authenticateUser ,uploadRoutes)
+
+
+
+
+
+// listen function & connect to database
+const port=3000
+const start=async()=>{
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port,()=>{
+            console.log(`database connected and listening on port ${port}` )
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+=======
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const connectDB = require('./db/connect');
+const authenticateUser = require('./middlewares/authenticate');
+require('dotenv').config();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+//import routes
+const uploadRoutes = require('./routes/upload');
+const authRoutes = require('./routes/auth');
+
+//routes
+app.use('/notsy/auth', authRoutes);
+app.use('/notsy', authenticateUser, uploadRoutes);
+
+// listen function & connect to database
+const port = 3000;
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () => {
+            console.log(`database connected and listening on port ${port}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+>>>>>>> 66978f4 (added chat app)
+
+start();
