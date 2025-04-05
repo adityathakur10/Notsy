0171dec70801ec35c4df19a874975caf83315930
aditya thakur <ad.2barwal@gmail.com>
2025-04-05 09:25:37 +0530

    delete

--- Backend/routes/folder.js
+++ Backend/routes/folder.js
@@ -4,8 +4,7 @@ const folderController=require('../controllers/notebook/folder');
 const upload=require('../config/multer');
 
 router.post('/',upload('./uploads/coverImages'),folderController.createFolder);
-// router.get('/',folderController.getAllFolders);
-// router.delete('/:id',folderController.deleteFolder);
+router.get('/',folderController.getAllFolders);
 router.delete('/:id',folderController.deleteFolder);
 
 module.exports=router;
